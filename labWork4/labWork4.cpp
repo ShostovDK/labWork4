f613aabbb724d00299f4a22649cca3f56bb0dfe4
F3llsy <dimaschostov2@gmail.com>
2023-05-22 01:53:27 +0300

    Добавьте файлы проекта.

---
+++ labWork4/labWork4.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+class MyList {
+private:
+    int* arr;
+    int size;
+public:
+    MyList() {
+        arr = nullptr;
+        size = 0;
+    }
+    MyList(int sz) {
+        arr = new int[sz];
+        size = sz;
+    }
+    ~MyList() {
+        delete[] arr;
+    }
+    int& operator[](int index) {
+        if (index >= 0 && index < size) {
+            return arr[index];
+        }
+        else {
+            cout << "Invalid index!" << endl;
+            exit(-1);
+        }
+    }
+};
+
+int main() {
+    MyList list(5);
+    list[0] = 10;
+    list[1] = 20;
+    list[2] = 30;
+    list[3] = 40;
+    list[4] = 50;
+
+    cout << list[0] << endl;
+    cout << list[2] << endl;
+
+    return 0;
+}
